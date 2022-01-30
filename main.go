bac91fd948e02a678f6b1165402f0a35cbb4d99a
Marius Merschformann <marius.merschformann@gmail.com>
2022-01-30 05:05:42 +0100

    Remove default config delete

--- main.go
+++ main.go
@@ -3,19 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-	"path"
 
 	"github.com/merschformann/gotz/core"
 )
 
 func main() {
-	// TODO: remove this
-	// Delete configuration file if it exists
-	home, _ := os.UserHomeDir()
-	configFile := path.Join(home, ".gotz.config.json")
-	if _, err := os.Stat(configFile); err == nil {
-		os.Remove(configFile)
-	}
 	// Get configuration
 	config, err := core.Load()
 	if err != nil {
