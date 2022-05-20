74915b8e4c7e27ca469479e828702a0abf53cd48
Marius Merschformann <marius.merschformann@gmail.com>
2022-05-20 02:38:37 +0200

    Adding notes option to release script

--- material/scripts/release.py
+++ material/scripts/release.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import os
 import subprocess
 
@@ -18,10 +19,11 @@ def get_version_from_git():
     return subprocess.check_output("git tag".split()).decode().strip().split("\n")[-1]
 
 
-def release(tag: str):
+def release(tag: str, notes: str = ""):
     """
     Release via github cli tool and show output as it occurs.
     """
+    # Prepare call
     call = [
         "gh",
         "release",
@@ -32,6 +34,14 @@ def release(tag: str):
         "--title",
         f"Release {tag}",
     ]
+    if notes:
+        call.extend(["--notes", f'"{notes}"'])
+    # Get user confirmation
+    print(" ".join(call))
+    if not input("Continue? [y/N] ").lower().startswith("y"):
+        print("Aborting.")
+        return
+    # Release
     with subprocess.Popen(
         call,
         stdout=subprocess.PIPE,
@@ -40,7 +50,6 @@ def release(tag: str):
     ) as p:
         for line in p.stdout:
             print(line, end="")  # process line here
-
     if p.returncode != 0:
         raise subprocess.CalledProcessError(p.returncode, p.args)
 
@@ -49,6 +58,11 @@ def main():
     """
     Main function.
     """
+    parser = argparse.ArgumentParser(description="Release a new version of gotz.")
+    parser.add_argument(
+        "--notes", "-n", help="Notes to add to the release.", default=""
+    )
+    args = parser.parse_args()
     git_version = get_version_from_git()
     go_version = get_version()
     print("Git version: {}".format(git_version))
@@ -57,7 +71,7 @@ def main():
         print("Cannot release an already released version.")
         return
     print(f"Releasing new version {go_version} ...")
-    release(go_version)
+    release(go_version, args.notes)
 
 
 if __name__ == "__main__":
